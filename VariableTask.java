0231bc6359da692fb007e35ec25532ebe202c7d1
Pavithra S <128320062+9150491150@users.noreply.github.com>
2024-12-16 16:50:15 +0530

    Day 1 part- 2

---
+++ VariableTask.java
@@ -0,0 +1,22 @@
+class VariableTask{
+public static void main(String[] args){
+int num=7;
+System.out.println("Integer:"+num);
+byte b=123;
+System.out.println("Byte:"+b);
+short s=74;
+System.out.println("Short:"+s);
+long l=1234567;
+System.out.println("Long:"+l);
+float f=12.43f;
+System.out.println("Float:"+f);
+double d=9840013313d;
+System.out.println("Double:"+d);
+char c='p';
+System.out.println("char:"+c);
+boolean b0=false;
+System.out.println("boolean:"+b0);
+String name="pavi";
+System.out.println("String:"+name);
+}
+}
