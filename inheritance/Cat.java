53e9d7885dc1bcb84b229d942346b705d59c643e
Pavithra S <128320062+9150491150@users.noreply.github.com>
2025-01-28 16:32:34 +0530

    day17

---
+++ inheritance/Cat.java
@@ -0,0 +1,8 @@
+package Inheritance2;
+
+public class Cat extends Animal{
+	public void makeSound() {
+		System.out.println("The Cat Meows");
+	}
+
+}
---
+++ inheritance/Inheritance2UI.java
@@ -0,0 +1,14 @@
+package Inheritance2;
+
+public class Inheritance2UI {
+	public static void main(String[]args) {
+		Dog d=new Dog();
+		d.makeSound();
+		
+		Cat c=new Cat();
+		c.makeSound();
+		
+	}
+	
+
+}
