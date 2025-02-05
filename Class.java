3aac9115af44b7f6aabdc18cd8db6656f843ad65
Pavithra S <128320062+9150491150@users.noreply.github.com>
2025-02-05 16:18:09 +0530

    day20-Wrapper

---
+++ Class.java
@@ -0,0 +1,49 @@
+package Wrapper;
+
+public class Class {
+	public static void main(String[]args) {
+		//Autoboxing
+		int num=7;
+		Integer num1=num;
+		//Unboxing
+		int num2=num1;
+		System.out.println(num2);
+		
+		byte b=90;
+		Byte b1=b;
+		byte b2=b1;
+		System.out.println(b2);
+		
+		float f=8;
+		Float f1=f;
+		float f2=f1;
+		System.out.println(f2);
+		
+		char c='p';
+		Character c1=c;
+		char c2=c1;
+		System.out.println(c2);
+		
+		short s=123;
+		Short s1=s;
+		short s2=s1;
+		System.out.println(s2);
+		
+		long l=123445;
+		Long l1=l;
+		Long l2=l1;
+		System.out.println(l2);
+		
+		double d=12.00;
+		Double d1=d;
+		double d2=d1;
+		System.out.println(d2);
+		
+		
+		
+		
+		
+		
+	}
+
+}
