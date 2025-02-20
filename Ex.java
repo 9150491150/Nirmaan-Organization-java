d529621c7c2922a7547f8b7c3afcc3c9238d3607
Pavithra S <128320062+9150491150@users.noreply.github.com>
2025-02-20 10:49:30 +0530

    ExceptionalHandling

--- Ex.java
+++ Ex.java
@@ -1,60 +1,65 @@
 package ExceptionalHandling;
 import java.util.Scanner;
 public class Ex {
-	void add(int a,int b) {
+	void add(int a, int b) {
 		System.out.println(a+b);
 	}
-	void sub(int a,int b) {
+
+	void sub(int a, int b) {
 		System.out.println(a-b);
 	}
-	void mul(int a,int b) {
+
+	void mul(int a, int b) {
 		System.out.println(a*b);
 	}
-	void div(int a,int b)throws Exception{
-		Thread.sleep(1000);
+
+	void div(int a, int b) throws InterruptedException{
+		Thread.sleep(7000);
 		System.out.println(a/b);
-		
+
 	}
-	public static void main(String[]args) {
-		Scanner sc=new Scanner(System.in);
+
+			
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
 		System.out.println("Enter your age:");
 		int age=sc.nextInt();
 		try {
-			if(age<=18) {
-				throw new Exception("Age is lessthan 18");
+			if(age<-18) { 
+				throw new ArithmeticException("Age is lessthan 18");
 			}
-		}catch(Exception e) {
-			System.out.println(e);
-		}finally {
+			}
+		catch(ArithmeticException e){
+				System.out.println(e);
+			}
+		finally {
 			System.out.println("Enter the correct age");
-		}
+			}
+		System.out.println("Enter the first number: ");
+		int fnum=sc.nextInt();
+		System.out.println("Enter the second number");
+		int scnum=sc.nextInt();
 		
-		
-		System.out.println("Enter num1:");
-		int num1=sc.nextInt();
-		System.out.println("Enter num2:");
-		int num2=sc.nextInt();
-		
-		Ex ob=new Ex();
-		ob.add(num1, num2);
-		ob.sub(num1, num2);
-		ob.mul(num1, num2);
-		
+		Ex ex1=new Ex();
+		ex1.add(fnum, scnum);
+		ex1.sub(fnum, scnum);
+		ex1.mul(fnum, scnum);
+
 		try {
-			ob.div(num1, num2);
-		}catch(Exception e) {
+			ex1.div(fnum,scnum);
+		}
+		catch(InterruptedException e) {
 			System.out.println(e);
 		}
+		catch(Exception e) {
+			System.out.println(e);
+			}
+		finally {
+			System.out.println("Division ended");
 		}
-		
-		
-	}
 	
-	
-	
-	
-	
-	
-	
-	
+
+}
+}
 
