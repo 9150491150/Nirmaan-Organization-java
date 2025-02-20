190ee61efed261cb3d60c712e2f404514f64464a
Pavithra S <128320062+9150491150@users.noreply.github.com>
2025-02-20 09:26:37 +0530

    Bank Management Project

---
+++ BankMgmtProj/Bank.java
@@ -0,0 +1,43 @@
+package BankMgmtSytm;
+
+public class Bank {
+	private int id= 22;
+	private String name="Pavi";
+	private double amount=700000;
+	private int pin=2025;
+	
+	
+	public Bank() {
+		
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	public int getPin() {
+		return pin;
+	}
+	
+	@Override
+	public String toString() {
+		return "Bank [id=" + id + ", name=" + name + ", amount=" + amount +"]";
+	}
+	
+}
+
+
